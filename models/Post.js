270f6370fbff7832269f16bfe43079251851d6ab
Sam Olander-Coffey <126676039+samolandercoffey@users.noreply.github.com>
2023-09-25 21:27:15 +0100

    Add files via upload

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  freefrom: {
+    type: String,
+    required: true,
+  },
+ location: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  notes: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
